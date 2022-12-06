833ebc171bd173cc15ba25d820877680907766cd
Abhimanyu Kumar <117224700+abhimanyu922@users.noreply.github.com>
2022-12-06 13:50:05 +0530

    Add files via upload

---
+++ demo1/src/demo1/democlass.java
@@ -0,0 +1,11 @@
+package demo1;
+
+public class democlass {
+	 public static void main(String[] args)
+	    {
+	        System.out.println("Welcome");
+	        System.out.println("To");
+	        System.out.println("demo project");
+	    }
+
+}
